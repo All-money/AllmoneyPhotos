6979dc232d7322379307595ce8eeb65a9a52fcc9
AlmanyBerthe <almanybert@gmail.com>
2025-05-14 12:07:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const imageFilenames = [
     '_MG_1167-1.jpg',
     '_MG_9255-1.jpg',
     '_MG_9270-1.jpg',
-    // Add more image filenames here
+    
   ];
   
   const galleryContainer = document.getElementById('gallery');
   
   imageFilenames.forEach((filename) => {
     const img = document.createElement('img');
-    img.src = `images/${filename}`;
+    img.src = `..images/${filename}`;
     img.alt = filename;
     img.loading = 'lazy';
     img.style.border = '1px solid #ccc';
